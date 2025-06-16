1481a7085e23ebb37d1d7378386bb651ff48081b
AndrewMcNeill7 <mcneillandrew088@gmail.com>
2025-06-16 17:35:37 -0400

    Revert "Project Progress 06.12.2025"
    This reverts commit f5acdfb356b023b106dcfb0c05758480284a83a9.

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -31,3 +31,4 @@ function Profile() {
 }
 
 export default Profile;
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,24 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './Components/CSS/Home.css';
+=======
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> parent of f5acdfb (Project Progress 06.12.2025)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
